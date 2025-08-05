ab5113647e8a305032b91912aa5626dbb1c9de95
Suzie Camaj <suziecamaj@yahoo.com>
2025-08-05 17:30:26 -0400

    fix: simplify integration test & quote Vercel secrets in CI

--- src/test/Cart.integration.test.jsx
+++ src/test/Cart.integration.test.jsx
@@ -1,8 +1,9 @@
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
-import store from '../store/store';
+import store from '../store/store';      
 import ProductCard from '../components/ProductCard';
 import Cart from '../components/Cart';
 
@@ -16,15 +17,14 @@ test('Cart updates when adding a product', () => {
 
   render(
     <Provider store={store}>
-      {/* show both product and cart side by side */}
       <ProductCard product={fakeProduct} />
       <Cart />
     </Provider>
   );
 
-  // click "Add to Cart" on our single ProductCard
+  // click our single Add to Cart button
   userEvent.click(screen.getByRole('button', { name: /add to cart/i }));
 
-  // the Cart component should now show our item's title
+  // Cart should now show the item’s title
   expect(screen.getByText(/Test Item/i)).toBeInTheDocument();
 });
